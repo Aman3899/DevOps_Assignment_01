3cce55d048920878567d63c41e645b1458006d49
AmanUllah <106026271+Aman3899@users.noreply.github.com>
2025-02-26 21:31:21 +0500

    Resolving CI CD errros #2

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,18 +1,18 @@
-export default [
-    {
-      ignores: ["node_modules/", "dist/"], // Ignore build folders
+module.exports = [
+  {
+    ignores: ["node_modules/", "dist/"], // Ignore build folders
+  },
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
-    {
-      languageOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-      },
-      plugins: {
-        import: require("eslint-plugin-import"),
-      },
-      rules: {
-        "no-unused-vars": "warn",
-        "import/no-unresolved": "error",
-      },
+    plugins: {
+      import: require("eslint-plugin-import"),
     },
-  ];  
+    rules: {
+      "no-unused-vars": "warn",
+      "import/no-unresolved": "error",
+    },
+  },
+];
